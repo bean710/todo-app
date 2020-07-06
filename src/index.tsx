25dce23d55be98ff11293732818997e7c9a99a6c
Ben Keener <keener4christ@gmail.com>
2020-07-05 20:52:21 -0400

    Changed to classes

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 const HelloComponent = () => {
@@ -9,26 +9,30 @@ interface ListItemProp {
   name: string;
 }
 
-const ListItem = (props: ListItemProp) => {
-  return <li>{props.name}</li>;
+class ListItem extends Component<ListItemProp, {}> {
+  render() {
+    return <li>{this.props.name}</li>;
+  }
 }
 
 interface ListProp {
   todos: string[];
 }
 
-const TodoList = (props: ListProp) => {
-  const todoNode = props.todos.map((todo) => {
-    return (<ListItem name={todo} key={todo} />);
-  });
-
-  return (<ul>{todoNode}</ul>);
+class TodoList extends Component<ListProp, {}> {
+  render() {
+    const todoNode = this.props.todos.map((todo) => {
+      return (<ListItem name={todo} key={todo} />);
+    });
+  
+    return (<ul>{todoNode}</ul>); 
+  }
 }
 
 ReactDOM.render(
   <React.StrictMode>
     <HelloComponent />
-    <TodoList todos={["a", "b", "c", "d"]} />
+    <TodoList todos={["a", "b", "c", "d", "e"]} />
   </React.StrictMode>,
   document.getElementById('root')
 );
